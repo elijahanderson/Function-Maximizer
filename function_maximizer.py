34b1897c73aeed2b7df86638573f28561d03b1d7
Elijah Anderson <elijahanderson_2019@depauw.edu>
2018-03-06 10:07:41 -0500

    Update 2018.3.6
    - Can now handle negative and multidigit coefficients
    - Updated formatting
    - Some minor improvements to algorithm
    TODO -- implement better crossover algorithm

--- function_maximizer.py
+++ function_maximizer.py
@@ -8,62 +8,79 @@
 
     Click this link to see the rest of the project explanation: https://i.gyazo.com/1a4b33ca025f5f0a6d1286a647c140ff.png
 
+    (Thede -- if you uncomment some of the lines of code, you can see how the program works step by step!)
 """
+
+# TODO -- implement a better crossover algorithm
 
 import operator
 import random
 import time
 
+# to calculate the values of each individual
 def calculate_values(pop, contents) :
     sum_dict = {}
-    for key, value in pop.items():
+    #print(contents)
+    for key, value in pop.items() :
         # separate individuals into their variables, convert to decimal, store them in a dictionary
         var_dict = {}
-        for i in range(num_vars):
+        for i in range(num_vars) :
             pos = i * len(value) // num_vars
             var_dict[i] = int(value[pos:pos + 4], 2)
-        # print('Individual ' + str(key) + ': ' + str(var_dict))
-
+        #print('Individual ' + str(key) + ': ' + str(var_dict))
         # read from the text file to compute the sum of each individual
         sum = 0
         for row, line in enumerate(contents) :
             var = 0
-            for col, coeff in enumerate(line) :
-                # ignore newlines and spaces
-                if coeff != '-' :
-                    # skip over coeffs that have already been counted
-                    if row > col :
-                        if col != 0 :
-                            var += 1
-                        continue
-                    # print('Row: ' + str(row) + ' Column: ' + str(col) + ', ' + coeff)
-                    # check to adjust the coeff if the previous element was a negative sign
-                    if line[col - 1] == '-' :
-                        coeff = -int(coeff)
-                        print('NEGATIVE COEFF: ' + str(coeff))
-                    # first coeff is never multiplied by anything
-                    if row == 0 and col == 0 :
-                        sum += int(coeff)
-                    # rest of first row is the coeff multiplied by just one appropriate variable
-                    elif row == 0 and col > 0 :
-                        sum += int(coeff) * var_dict[var]
+            eles = line.split(' ')
+            #print(eles)
+            for col, coeff in enumerate(eles) :
+                # skip over coeffs that have already been counted
+                if row > col:
+                    if col != 0:
                         var += 1
-                    # all other coeffs
-                    elif col > 0 :
-                        sum += int(coeff) * var_dict[row - 1] * var_dict[var]
-                        var += 1
+                    continue
+                #print('Row: ' + str(row) + ' Column: ' + str(col) + ', ' + coeff + ', ' + str(var))
+                # first coeff is never multiplied by anything
+                if row == 0 and col == 0:
+                    sum += int(coeff)
+                # rest of first row is the coeff multiplied by just one appropriate variable
+                elif row == 0 and col > 0:
+                    sum += int(coeff) * var_dict[var]
+                    var += 1
+                # all other coeffs
+                elif col > 0:
+                    sum += int(coeff) * var_dict[row-1] * var_dict[var]
+                    var += 1
 
-        # print('Sum of individual ' + str(key) + ': ' + str(sum))
+        #print('Sum of individual ' + str(key) + ': ' + str(sum))
+        #time.sleep(10)
         sum_dict[key] = sum
     return sum_dict
-# to evaluate each individual
+
+# to evaluate the fitness of each population
 def evaluate(pop) :
-    print('=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+    print('=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
     print(pop)
+    # get length of each bit string, this will be used later
+    bit_length = len(pop[0])
     # evaluate the individual
     sum_dict = calculate_values(pop, contents4)
     print('The sums: ' + str(sum_dict))
-    # to return later
+
+    # if any of the sums are below 0, add the appropriate amount to each which makes the lowest neg value == 1
+    # smallest = 0
+    # to_add = False
+    # for idv, sum in sum_dict.items() :
+    #     if sum < 1 and sum < smallest :
+    #         smallest = sum
+    #         to_add = True
+    # #print('Smallest negative sum: ' + str(smallest))
+    # if to_add :
+    #     for idv, sum in sum_dict.items() :
+    #         sum_dict[idv] += -(smallest-1)
+    #     print('Adjusted sums: ' + str(sum_dict))
+    # to return later -- the expression returns the key of the largest value in sum_dict
     best = pop[max(sum_dict.items(), key=operator.itemgetter(1))[0]]
 
     # using the sum_dict, choose which the best two individuals are passed into the next generation
@@ -71,13 +88,14 @@ def evaluate(pop) :
     new_pop = {}
     A = max(sum_dict.items(), key=operator.itemgetter(1))[0]
     new_pop[0] = pop[A]
+    # remove largest, find next largest
     sum_dict.pop(A)
     B = max(sum_dict.items(), key=operator.itemgetter(1))[0]
     new_pop[1] = pop[B]
 
     # we have a new population with the max two individuals of the old population
     #print(new_pop)
-    # time to perform cross_over... select two points randomly within the bit string
+    # time to perform cross-over... select two points randomly within the bit string
     cross_pop = {}
     x = random.randint(0, len(new_pop[0]))
     y = random.randint(0, len(new_pop[0]))
@@ -87,18 +105,13 @@ def evaluate(pop) :
 
     # each individual will get a portion of one of the two top individuals
     # the top two individuals may remain the same, but that's fine
-    #print('The old population: ' + str(pop))
     for i in range(num_individuals) :
         j = random.randint(0, 1)
         # swap from top individual
-        if j == 0 :
+        if j == 0:
             cross_pop[i] = pop[i][:start]+new_pop[0][start:end]+pop[i][end:]
-        else :
+        else:
             cross_pop[i] = pop[i][:start]+new_pop[1][start:end]+pop[i][end:]
-
-    #cross_pop[0] = new_pop[0][:start]+new_pop[1][start:end]+new_pop[0][end:]
-    #cross_pop[1] = new_pop[1][:start]+new_pop[0][start:end]+new_pop[1][end:]
-    #print('The crossed over population: ' + str(cross_pop))
 
     # now for some fun -- it's time for mutation!
     #print('Before mutation: ' + str(cross_pop))
@@ -108,20 +121,22 @@ def evaluate(pop) :
         for i in range(len(value)) :
             # every bit will have a small chance of being flipped
             mut_chance = random.randint(0, 50)
-            if mut_chance == 0 :
-                if cross_pop[idv][i] == '0' :
+            if mut_chance == 0:
+                if cross_pop[idv][i] == '0':
                     mut_pop[idv] += '1'
-                else :
+                else:
                     mut_pop[idv] += '0'
                 mut_pop[idv] += cross_pop[idv][i]
-            else :
+            else:
                 mut_pop[idv] += cross_pop[idv][i]
-            if len(mut_pop[idv]) == 12 :
+            if len(mut_pop[idv]) == bit_length:
                 break
     #print('After mutation:  ' + str(mut_pop))
-
     return mut_pop, best
 
+########################################################################################################################
+# MAIN #################################################################################################################
+########################################################################################################################
 
 print('Welcome to Eli\'s function maximizer! Please enter the text file you want read: ', end='')
 filename = input().strip()
@@ -129,12 +144,12 @@ generations = 30
 num_individuals = 100
 
 try :
-    with open(filename, 'r') as file :
+    with open(filename, 'r') as file:
         # retrieve file contents, set each line as an element in a list
         contents = file.readlines()
         contents2 = contents[2:]
-        # strip unnecessary whitespaces
-        contents3 = [line.replace(' ', '') for line in contents2]
+        # strip double spaces, replace with spaces
+        contents3 = [line.replace('  ', ' ') for line in contents2]
         # strip newlines
         contents4 = [line.strip() for line in contents3]
         # get the number of variables
@@ -156,45 +171,43 @@ try :
         for i in range(generations) :
             pop, best = evaluate(pop)
             print('The new population: ' + str(pop))
-            print('The best performing number: ' + best)
+            print('The best performing individual: ' + best)
             time.sleep(1)
 
         # using the best binary number, print the best set of variables found
         best_dict = {}
-        for i in range(num_vars):
+        for i in range(num_vars) :
             pos = i * len(best) // num_vars
             best_dict[i] = int(best[pos:pos + 4], 2)
+        print('=======================================================================================================')
         print('The best variables -- imagine there\'s an x in front of each number key: ' + str(best_dict))
+
         # print the value of the function with those variables
         sum = 0
-        for row, line in enumerate(contents4):
+        for row, line in enumerate(contents4) :
             var = 0
-            for col, coeff in enumerate(line):
-                # ignore newlines and spaces
-                if coeff != '-':
-                    # skip over coeffs that have already been counted
-                    if row > col:
-                        if col != 0:
-                            var += 1
-                        continue
-                    # print('Row: ' + str(row) + ' Column: ' + str(col) + ', ' + coeff)
-                    # check to adjust the coeff if the previous element was a negative sign
-                    if line[col - 1] == '-':
-                        coeff = -int(coeff)
-                        print('NEGATIVE COEFF: ' + str(coeff))
-                    # first coeff is never multiplied by anything
-                    if row == 0 and col == 0:
-                        sum += int(coeff)
-                    # rest of first row is the coeff multiplied by just one appropriate variable
-                    elif row == 0 and col > 0:
-                        sum += int(coeff) * best_dict[var]
+            eles = line.split(' ')
+            #print(eles)
+            for col, coeff in enumerate(eles) :
+                # skip over coeffs that have already been counted
+                if row > col:
+                    if col != 0:
                         var += 1
-                    # all other coeffs
-                    elif col > 0:
-                        sum += int(coeff) * best_dict[row - 1] * best_dict[var]
-                        var += 1
+                    continue
+                #print('Row: ' + str(row) + ' Column: ' + str(col) + ', ' + coeff + ', ' + str(var))
+                # first coeff is never multiplied by anything
+                if row == 0 and col == 0:
+                    sum += int(coeff)
+                # rest of first row is the coeff multiplied by just one appropriate variable
+                elif row == 0 and col > 0:
+                    sum += int(coeff) * best_dict[var]
+                    var += 1
+                # all other coeffs
+                elif col > 0:
+                    sum += int(coeff) * best_dict[row-1] * best_dict[var]
+                    var += 1
 
         print('Value of the function with those variables: ' + str(sum))
 
 except FileNotFoundError :
-    print('No file of that name detected; program termintated.')
+    print('No file of that name detected; program terminated.')

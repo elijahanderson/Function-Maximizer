b3f0a015fdd2371a5859b9ed4e6443333976df5d
Elijah Anderson <elijahanderson_2019@depauw.edu>
2018-03-21 20:52:51 -0400

    Clean-up
    - Added 8-bit variables
    - cleaned up code
    - added rand10.txt

--- function_maximizer.py
+++ function_maximizer.py
@@ -11,8 +11,6 @@
     (Thede -- if you uncomment some of the lines of code, you can see how the program works step by step!)
 """
 
-# TODO -- implement a better crossover algorithm
-
 import operator
 import random
 import time
@@ -28,7 +26,7 @@ def calculate_values(pop, contents) :
         var_dict = {}
         for i in range(num_vars) :
             pos = i * len(value) // num_vars
-            var_dict[i] = int(value[pos:pos + 4], 2)
+            var_dict[i] = int(value[pos:pos + 8], 2)
         #print('Individual ' + str(key) + ': ' + str(var_dict))
         # read from the text file to compute the sum of each individual
         sum = 0
@@ -67,25 +65,13 @@ def evaluate(pop) :
     print('=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
     print(pop)
     # evaluate the individual
-    sum_dict = calculate_values(pop, contents4)
+    sum_dict = calculate_values(pop, contents6)
     print('The sums: ' + str(sum_dict))
 
-    # if any of the sums are below 0, add the appropriate amount to each which makes the lowest neg value == 1
-    # smallest = 0
-    # to_add = False
-    # for idv, sum in sum_dict.items() :
-    #     if sum < 1 and sum < smallest :
-    #         smallest = sum
-    #         to_add = True
-    # #print('Smallest negative sum: ' + str(smallest))
-    # if to_add :
-    #     for idv, sum in sum_dict.items() :
-    #         sum_dict[idv] += -(smallest-1)
-    #     print('Adjusted sums: ' + str(sum_dict))
     # to return later -- the expression returns the key of the largest value in sum_dict
     best = pop[max(sum_dict.items(), key=operator.itemgetter(1))[0]]
 
-    # using the sum_dict, choose which the best two individuals are passed into the next generation
+    # using the sum_dict, choose which the best individuals are passed into the next generation
     #print(pop)
     new_pop = {}
     A = max(sum_dict.items(), key=operator.itemgetter(1))[0]
@@ -94,7 +80,12 @@ def evaluate(pop) :
     sum_dict.pop(A)
     B = max(sum_dict.items(), key=operator.itemgetter(1))[0]
     new_pop[1] = pop[B]
-
+    C = max(sum_dict.items(), key=operator.itemgetter(1))[0]
+    new_pop[2] = pop[C]
+    D = max(sum_dict.items(), key=operator.itemgetter(1))[0]
+    new_pop[3] = pop[D]
+    E = max(sum_dict.items(), key=operator.itemgetter(1))[0]
+    new_pop[4] = pop[E]
     # we have a new population with the max two individuals of the old population
     #print(new_pop)
     # time to perform cross-over... select two points randomly within the bit string
@@ -106,14 +97,11 @@ def evaluate(pop) :
     #print('Slicing from ' + str(start) + ' to ' + str(end))
 
     # each individual may get a portion of one of the two top individuals
-    # the top two individuals may remain the same, but that's fine
+    # the top individuals may remain the same, but that's fine
     for i in range(num_individuals) :
-        j = random.randint(0, 1)
-        # swap from top individual
-        if j == 0:
-            cross_pop[i] = pop[i][:start]+new_pop[0][start:end]+pop[i][end:]
-        else:
-            cross_pop[i] = pop[i][:start]+new_pop[1][start:end]+pop[i][end:]
+        j = random.randint(0, 4)
+        # swap from top individuals
+        cross_pop[i] = pop[i][:start]+new_pop[j][start:end]+pop[i][end:]
 
     # now for some fun -- it's time for mutation!
     #print('Before mutation: ' + str(cross_pop))
@@ -123,10 +111,9 @@ def evaluate(pop) :
         for i in range(len(value)) :
             if len(mut_pop[idv]) == bit_length:
                 break
-            # every bit will have a small chance of being flipped -- various testing has convinved me that a 1/50 chance
-            # is optimal
+            # every bit will have a small chance of being flipped -- various testing has convinced me that a 1/150 chance is optimal
             else :
-                mut_chance = random.randint(0, 50)
+                mut_chance = random.randint(0, 150)
                 if mut_chance == 0:
                     if cross_pop[idv][i] == '0':
                         mut_pop[idv] += '1'
@@ -144,18 +131,22 @@ def evaluate(pop) :
 
 print('Welcome to Eli\'s function maximizer! Please enter the text file you want read: ', end='')
 filename = input().strip()
-generations = 1000
-num_individuals = 100
+generations = 1750
+num_individuals = 1000
 
 try :
     with open(filename, 'r') as file:
         # retrieve file contents, set each line as an element in a list
         contents = file.readlines()
         contents2 = contents[2:]
-        # strip double spaces, replace with spaces
-        contents3 = [line.replace('  ', ' ') for line in contents2]
+        # strip extra spaces, replace with spaces
+        contents3 = [line.replace('    ', ' ') for line in contents2]
+        contents4 = [line.replace('   ', ' ') for line in contents3]
+        contents5 = [line.replace('  ', ' ') for line in contents4]
         # strip newlines
-        contents4 = [line.strip() for line in contents3]
+        contents6 = [line.strip() for line in contents5]
+        print(contents6)
+        #time.sleep(10)
         # get the number of variables
         num_vars = int(contents[0])
         print(num_vars)
@@ -168,31 +159,44 @@ try :
         # fill each individual with the appropriate number of random bits
         for key in pop:
             for i in range(num_vars) :
-                pop[key] += str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1))
+                pop[key] += str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1)) + \
+                            str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1)) + str(random.randint(0, 1))
             print(pop[key])
 
         # get length of each bit string, this will be used later
         bit_length = len(pop[0])
 
+        # put all the best individuals from each iteration into this dictionary:
+        best_pop = {}
+
         # run the algorithm
         for i in range(generations) :
             pop, best = evaluate(pop)
+            best_pop[i] = best
             print('The new population: ' + str(pop))
             print('The best performing individual: ' + best)
             # uncomment for periodic updates
             #time.sleep(1)
 
+        print('=======================================================================================================')
+
+        print('The best individual from every generation: ' + str(best_pop))
+
+        # calculate the sums of the best individual from each generation
+        best_sums = calculate_values(best_pop, contents6)
+        # get the overall best indidvidual
+        best_idv = best_pop[max(best_sums.items(), key=operator.itemgetter(1))[0]]
+
         # using the best binary number, print the best set of variables found
         best_dict = {}
-        for i in range(num_vars) :
-            pos = i * len(best) // num_vars
-            best_dict[i] = int(best[pos:pos + 4], 2)
-        print('=======================================================================================================')
+        for i in range(num_vars):
+            pos = i * len(best_idv) // num_vars
+            best_dict[i] = int(best_idv[pos:pos + 8], 2)
         print('The best variables -- imagine there\'s an x in front of each number key: ' + str(best_dict))
 
         # print the value of the function with those variables
         sum = 0
-        for row, line in enumerate(contents4) :
+        for row, line in enumerate(contents6) :
             var = 0
             eles = line.split(' ')
             #print(eles)
